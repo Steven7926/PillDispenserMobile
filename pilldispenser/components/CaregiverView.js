0feca8cfd2a10f94a764adc8e71a6007211e7257
steve <steve@DESKTOP-RFPL45I>
2021-02-19 18:53:49 -0500

    new commit

--- pilldispenser/components/CaregiverView.js
+++ pilldispenser/components/CaregiverView.js
@@ -14,16 +14,20 @@ class CaregiverView extends React.Component {
     }
 
     state = {
-        modalVisible: false
-
+        modalVisible: false,
+        modalDel: false
     };
 
     setModalVisible = (visible) => {
         this.setState({ modalVisible: visible });
     }
+    setDelModalVisible = (visible) => {
+        this.setState({ modalDel: visible });
+    }
 
     render() {
         const { modalVisible } = this.state;
+        const { modalDel } = this.state;
         return (
             <View style={styles.caregiverContainer}>
                 <Text style={{ marginBottom: 5, fontSize: 20, fontWeight: "bold" }}> Name: {this.props.carename}</Text>
@@ -77,6 +81,35 @@ class CaregiverView extends React.Component {
                             </TouchableOpacity>
                         </View>
                         </View>
+                </Modal>
+
+
+
+                <Modal visible={modalDel} animationType='slide'>
+                    <View style={styles.signupcontainer}>
+                        <View style={{ marginTop: 15, width: 350 }}>
+                            <TouchableOpacity
+                                style={styles.loginbut}
+                                activeOpacity={.5}
+                            //onPress={addMedication}
+                            >
+                                <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                                    <FontAwesomeIcon icon={faCheck} size={20} style={{ color: '#ffffff' }} />
+                                    <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'white' }}> Yes</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.closebutton}
+                                activeOpacity={.5}
+                                onPress={() => this.setDelModalVisible(!modalVisible)}
+                            >
+                                <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                                    <FontAwesomeIcon icon={faWindowClose} size={20} style={{ color: '#ffffff' }} />
+                                    <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'white' }}> No</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </Modal>
             </View>
         );
